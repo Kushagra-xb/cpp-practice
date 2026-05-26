14f611925ee47ac1109158b430e640fe62e98062
Kushagra-xb <codexboyz00@gmail.com>
2026-05-26 13:21:06 +0530

    Arrays DSA 1

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 int main(){
-    int arr[5];
-    cout<<"Enter marks of 5 subjects: "<<endl;
-    for(int i=0;i<5;i++){
-        cin>>arr[i];
+    int arr[5]={1,2,3,4,5};
+    int left = 0;
+    int right=4;
+
+    while(left<right){
+        int temp = arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
     }
-    int n;
-    cout<<"Enter the subject number to display marks: "<<endl;
-    cin>>n;
-    cout<<"Marks of subject "<<n<<" is: "<<arr[n-1]<<endl;
-    return 0;
-   
+    for (int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
 }
