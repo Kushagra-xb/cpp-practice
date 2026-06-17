2670f9f88fee5abf448883c5b30543c282cb162c
Kushagra-xb <codexboyz00@gmail.com>
2026-06-17 15:05:46 +0530

    decimal to binary

--- basics/binary.cpp
+++ basics/binary.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int btodec(int n){
-    int ans = 0, pow =1;
-    while (n>0){
-        int rem = n%10;
-    ans += rem*pow;
-    n /=10;
-    pow *= 2;
-    
-} return ans;
-}
+int dectoB(int decnum){
+    int ans =0,pow =1;
+    while (decnum>0){
+
+        int rem = decnum%2;
+        decnum /= 2;
+        
+        ans += rem*pow;
+        pow *= 10;;
+       
+
+    } return ans; 
+
+} 
 int main(){
-    int n;
-    cout<<"Enter a binary number: ";
-    cin>>n;
-    cout<<btodec(n)<<endl;
-    return 0;
+
+   for(int i=2;i<=50;i++){
+    cout<<"The binary equivalent of "<<i<<" is: "<<dectoB(i)<<endl;
+   }
+   
+   return 0;  
 }
