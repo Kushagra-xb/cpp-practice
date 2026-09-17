e6651c54605e11b90ab174b39d7a352229c3e095
Kushagra-xb <codexboyz00@gmail.com>
2026-09-18 00:23:40 +0530

    user profile creation using getline

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int main(){
+ cout<<"enter your name = "<<endl;
+    string name;
+ getline(cin,name);
  int age;
- double height;
- char grade;
- cout<<"Enter your Age = "<<endl;cin>>age;
- cout<<"Enter your height = "<<endl;cin>>height;
- cout<<"enter your grade= "<<endl;cin>>grade;
+ double cgpa;
+ char sec;
+ cout<<"enter your age = "<<endl;cin>>age;
+ cout<<"enter your cgpa = "<<endl;cin>>cgpa;
+ cout<<"enter your section = "<<endl;cin>>sec;
 
- cout<<"your age,height and grade are "<<age<<", "<<height<<" and "<<grade;
+ cout<<"Your Profile is -->"<<endl;
+ cout<<"Name \t = \t"<<name<<endl;
+ cout<<"Age \t = \t"<<age<<endl;
+ cout<<"CGPA \t = \t"<<cgpa<<endl;
+ cout<<"Section  = \t"<<sec;
 
 
-     
+     return 0;
 }
