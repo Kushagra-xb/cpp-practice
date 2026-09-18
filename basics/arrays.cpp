490ba0ed42de07945e2b98175fef8a05970231f4
Kushagra-xb <codexboyz00@gmail.com>
2026-09-19 01:27:36 +0530

    switch calculator

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -1,23 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
- int u;
- int bill;
- cout<<"how many units of elec consumed = "<<endl;cin>>u;
- if(u<=100){
-   bill=u*5;
-   cout<<"your elec bill is "<<bill<<endl;
-   return 0;
- }else if(u<=200){
-   bill=u*7;
-    cout<<"your elec bill is "<<bill<<endl;
+int main() {
+    int a, b;
+    float c;
+    char op;
+
+    cout << "enter a number = ";
+    cin >> a;
+    cout << "choose the operation = ";
+    cin >> op;
+    cout << "give another number = ";
+    cin >> b;
+   
+
+    switch (op) {
+        case '+':
+            cout << "sum of "<<a<<" and "<<b<<" is " <<a + b;
+            break;
+        case '-':
+            cout << "differance of "<<a<<" and "<<b<<" is " <<a - b;
+            break;
+        case '*':
+            cout << "multiplication of "<<a<<" and "<<b<<" is " <<a * b;
+            break;
+        case '/':
+             cout << "division of "<<a<<" and "<<b<<" is " <<a / b;
+            break;
+         case '%':
+         c=a%b;
+          cout << "remainder of "<<a<<" and "<<b<<" is " <<c;
+         
+          break;
+        default:
+            cout << "Invalid operator";
+    }
+
     return 0;
- }else
-   {
-      bill=u*10;
-      cout<<"your elec bill is "<<bill<<endl;
-    return 0;
-   }
-     return 0;
 }
