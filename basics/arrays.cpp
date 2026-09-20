343c8468fbcb1a6eb34cddbc5341bf21563fe5e4
Kushagra-xb <codexboyz00@gmail.com>
2026-09-20 16:18:30 +0530

    easy lvl que done

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -2,17 +2,12 @@
 using namespace std;
 
 int main() {
- int n;
- int sum=0;
+ int n,m=0;
  cout<<"enter any no = "<<endl;
  cin>>n;
- if(n>0){
-  cout<<n<<" is a positive no."<<endl;
-  for(int i=1;i<=n;i++){
-    cout<<i<<" "<<endl;
-    sum+=i;
-  }cout<<"sum is "<<sum;
- } else if (n<0){cout<<n<<" is negative no."<<endl;
- }else cout<<n<<"is zero.";
+while(n!=0){
+ n=n/10;
+m++;
+}cout<<m;
     return 0;
 }
