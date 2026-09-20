83851c21b8b54dfbd6e5b2a67b7a57958f5601a7
Kushagra-xb <codexboyz00@gmail.com>
2026-09-20 15:47:01 +0530

    revison

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -2,39 +2,17 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    float c;
-    char op;
-
-    cout << "enter a number = ";
-    cin >> a;
-    cout << "choose the operation = ";
-    cin >> op;
-    cout << "give another number = ";
-    cin >> b;
-   
-
-    switch (op) {
-        case '+':
-            cout << "sum of "<<a<<" and "<<b<<" is " <<a + b;
-            break;
-        case '-':
-            cout << "differance of "<<a<<" and "<<b<<" is " <<a - b;
-            break;
-        case '*':
-            cout << "multiplication of "<<a<<" and "<<b<<" is " <<a * b;
-            break;
-        case '/':
-             cout << "division of "<<a<<" and "<<b<<" is " <<a / b;
-            break;
-         case '%':
-         c=a%b;
-          cout << "remainder of "<<a<<" and "<<b<<" is " <<c;
-         
-          break;
-        default:
-            cout << "Invalid operator";
-    }
-
+ int n;
+ int sum=0;
+ cout<<"enter any no = "<<endl;
+ cin>>n;
+ if(n>0){
+  cout<<n<<" is a positive no."<<endl;
+  for(int i=1;i<=n;i++){
+    cout<<i<<" "<<endl;
+    sum+=i;
+  }cout<<"sum is "<<sum;
+ } else if (n<0){cout<<n<<" is negative no."<<endl;
+ }else cout<<n<<"is zero.";
     return 0;
 }
