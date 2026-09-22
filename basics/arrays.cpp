6a5155e3e3776bf2bcf6e70ee61f83d5db4d2f12
Kushagra-xb <codexboyz00@gmail.com>
2026-09-22 22:55:11 +0530

    Added ATM withdrawal practice program

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -1,13 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
- int n,m=0;
- cout<<"enter any no = "<<endl;
- cin>>n;
-while(n!=0){
- n=n/10;
-m++;
-}cout<<m;
+int main(){
+
+int w,b=1432;
+int a,c;
+cout<<"press 1 - check balance: "<<endl;
+cout<<"press 2 - withdraw: "<<endl;
+cin>>a;
+if(a==1){
+    cout<<"Your Account Balance is: $"<<b<<endl;
     return 0;
+
+}else{
+    cout<<"Enter the amount: "<<endl;
+    cin>>w;}
+    if( w%5!=0 || w+0.5>b){
+        cout<<"invalid amount.."<<endl;
+        return 0;
+    }
+    cout<<"enter PIN: "<<endl;
+    cin>>c;
+    if(c==1234){
+        b-0.5;
+            cout<<"Your Amount of "<<w<<" is been withdrawal."<<endl;
+            cout<<"Charge of 0.5 is been deducted from Account."<<endl;
+            cout<<"your current balance is: $"<<b;
+            return 0;
+    }else {cout<<"invalid pin.."<<endl;
+    }
+
+return 0; 
 }
